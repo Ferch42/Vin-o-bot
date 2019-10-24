452bbf80d9c6623770f9cde57fed9c450932938c
Ferch42 <fernando.hsieh@usp.br>
2019-10-24 13:16:05 +0200

    doneee

--- main.js
+++ main.js
@@ -1,62 +1,5 @@
 angular.module('KRRclass', [ 'chart.js']).controller('MainCtrl', ['$scope','$http', mainCtrl]);
 
-//var uluru = {lat: 52.334518, lng: 4.866753};
-
-var globalOne =  {lat: 52.334518, lng: 4.866753};
-var wine_list  = []
-
-function initMap() {
-  // The location of Uluru
-  //var uluru = {lat: 52.334518, lng: 4.866753};
-
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 6, center: globalOne});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: globalOne, map: map});
-}
-
-
-
-
-
-
-/////////////////////////////////
-
-
-function onRowClick(tableId, callback) {
-    var table = document.getElementById(tableId),
-        rows = table.getElementsByTagName("tr"),
-        i;
-        console.log("value>>");
-    for (i = 0; i < rows.length; i++) {
-        table.rows[i].onclick = function (row) {
-            return function () {
-                callback(row);
-            };
-        }(table.rows[i]);
-    }
-};
- 
-onRowClick("winetable", function (row){
-    var value = row.getElementsByTagName("td")[0].innerHTML;
-    document.getElementById('click-response').innerHTML = value + " clicked!";
-    console.log("value>>", value);
-});
-
-function abacaxi(){
-	console.log(this)
-}
-
-
-
-
-
-
-
-//////////////////////////
-
-
 
 function mainCtrl($scope, $http, ChartJsProvider){
   	
@@ -68,7 +11,7 @@ function mainCtrl($scope, $http, ChartJsProvider){
 	//$scope.map_coordinates = {lat: 52.334518, lng: 4.866753};
 
 
-	$scope.myendpoint = "http://localhost:7200/repositories/final?query=";
+	$scope.myendpoint = "http://localhost:7200/repositories/hiper_mega_final_winez?query=";
 	
 
 
@@ -108,6 +51,8 @@ function mainCtrl($scope, $http, ChartJsProvider){
 			PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#> 
 			PREFIX cbo:<http://comicmeta.org/cbo/> 
 			PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+			PREFIX dbr: <http://dbpedia.org/resource/>
+			PREFIX gn: <http://www.geonames.org/ontology#>
 			select distinct ?winelabel ?taste_profile ?winerylabel ?grapelabel ?rating ?price where { 
 			 `;
 
@@ -116,16 +61,17 @@ function mainCtrl($scope, $http, ChartJsProvider){
 				$scope.query_string = $scope.query_string + " ?wine ex:hasTasteProfile ex:"+selected_tastes[j].value+ " ."
 			}
 			$scope.query_string = $scope.query_string +  " ?wine rdf:type "+colour_input + " . ";
+			$scope.query_string = $scope.query_string +  " ?winery gn:locatedIn ex:"+ location_input+ " . ";
 			//$scope.query_string = $scope.query_string +  " ?wine rdf:type "+colour_input + " . ";
-
+			$scope.query_string = $scope.query_string +  " ?wine rdf:type ex:"+ price_input+ " . ";
 			$scope.query_string = $scope.query_string +  `?wine rdfs:label ?winelabel .
-    ?wine ex:concatString ?taste_profile .
+    ?wine ex:concatstring ?taste_profile .
     ?wine vin:hasMaker ?winery .
     ?winery rdfs:label ?winerylabel .
     ?wine vin:madeFromGrape ?grape  .
     ?grape rdfs:label ?grapelabel.
     ?wine ex:hasRating ?rating .
-    ?wine cbo:price ?price . } order by DESC(` +sorting_input+ ") limit 100";
+    ?wine ex:exactprice ?price . } order by ` +sorting_input+ ") limit 50";
 
 
 
@@ -134,8 +80,8 @@ function mainCtrl($scope, $http, ChartJsProvider){
     		console.log($scope.dinamic_string)
     		$http( {
  	method: "GET",
-	url : $scope.myendpoint+  $scope.dinamic_string 
-	//headers : {'Accept':'application/sparql-results+json', 'Content-Type':'application/sparql-results+json'}
+	url : $scope.myendpoint+  $scope.dinamic_string , 
+	headers : {'Accept':'application/sparql-results+json', 'Content-Type':'application/sparql-results+json'}
 	} )
 	.success(function(data, status ) {
 	      // TODO : your code here 
@@ -145,15 +91,17 @@ function mainCtrl($scope, $http, ChartJsProvider){
 	      console.log($scope.resultz["head"]["vars"])
 			if (!winetable) return ;
 
-			
+			if($scope.resultz["results"]["bindings"].length == 0){
+				alert('0 results available for this query')
+			}
 
-			var test = [1,2,3,4,5,6];
+			// var test = [1,2,3,4,5,6];
 			for (j=0 ; j<$scope.resultz["results"]["bindings"].length;j++){
 				var newRow = winetable.insertRow(winetable.rows.length);
 
 				for (i = 0; i < $scope.resultz["head"]["vars"].length; i++){
 					var cell = newRow.insertCell(i);
-					cell.innerHTML = $scope.resultz["results"]["bindings"][j][$scope.resultz["head"]["vars"][i]]["value"];
+					cell.innerHTML = $scope.resultz["results"]["bindings"][j][$scope.resultz["head"]["vars"][i]]["value"].replace("http://www.example.com/K&D/group42/", "");
 				}
 			}
 			var cells = winetable.getElementsByTagName('td');
@@ -177,14 +125,57 @@ function mainCtrl($scope, $http, ChartJsProvider){
 			            rowSelected.style.backgroundColor = "yellow";
 			            rowSelected.className += " selected";
 
-			            $scope.lat_lng_query = " PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> select ?lat ?lng where { ?s rdfs:label "+rowSelected.cells[0].innerHTML+" . ?s }"
-			            var map = new google.maps.Map(document.getElementById('map'), {
-        						center: {lat: 52.334518, lng: 4.866753},
+			            //$scope.lat_lng_query = " PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> select ?lat ?lng where { ?s rdfs:label '"+rowSelected.cells[0].innerHTML+" . ?s }"
+			            //$scope.lat_lng_query = " PREFIX gn: <http://www.geonames.org/ontology#>PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>PREFIX vin:<http://www.w3.org/TR/2003/PR-owl-guide-20031209/wine#> PREFIX dbo: <http://dbpedia.org/ontology/> select ?lat ?long ?grape ?image where { ?wine rdfs:label '"+rowSelected.cells[0].innerHTML + "' .  ?wine vin:hasMaker ?winery . ?winery gn:locatedIn ?region .  ?region geo:lat ?lat; geo:long ?long . ?wine vin:madeFromGrape ?grape}   SERVICE <http://dbpedia.org/sparql>{  	?grape dbo:thumbnail ?image .  }"
+			            $scope.lat_lng_query = ` PREFIX gn: <http://www.geonames.org/ontology#>PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX vin:<http://www.w3.org/TR/2003/PR-owl-guide-20031209/wine#> 
+PREFIX dbo: <http://dbpedia.org/ontology/>
+select ?lat ?long ?grape ?image where { 
+  ?wine rdfs:label '`+rowSelected.cells[0].innerHTML+`' .  
+  ?wine vin:hasMaker ?winery . 
+  ?winery gn:locatedIn ?region .  
+  ?region geo:lat ?lat; geo:long ?long . 
+  ?wine vin:madeFromGrape ?grape .
+  SERVICE <http://dbpedia.org/sparql>{
+  	?grape dbo:thumbnail ?image .
+  }
+}`
+
+
+			            console.log($scope.lat_lng_query)
+			            $scope.lat_lng_query = encodeURIComponent($scope.lat_lng_query)
+			            $http( {
+ 						method: "GET",
+							url : $scope.myendpoint+  $scope.lat_lng_query , 
+							headers : {'Accept':'application/sparql-results+json', 'Content-Type':'application/sparql-results+json'}
+						} ).success(function(data, status ){
+
+
+							$scope.lat_resultz = eval(data) ;
+							$scope.lat = 0 ;
+							$scope.long = 0 ;
+							if($scope.lat_resultz ["results"]["bindings"].length == 0){
+								alert('no location available')
+							}
+
+							else{
+								console.log($scope.lat_resultz ["results"]["bindings"][0]["grape"]["value"]) ;
+							   var map = new google.maps.Map(document.getElementById('map'), {
+        						center: {lat: Number($scope.lat_resultz ["results"]["bindings"][0]["lat"]["value"]), lng: Number($scope.lat_resultz ["results"]["bindings"][0]["long"]["value"])},
       						  zoom: 6
-     					 });
-						 
-						 var grape_image = document.getElementById("grapeimage");
-						 grape_image.src = "https://imgflip.com/s/meme/Mocking-Spongebob.jpg"; 
+     					 	});
+						 		var grape_image = document.getElementById("grapeimage");
+						 grape_image.src = $scope.lat_resultz ["results"]["bindings"][0]["image"]["value"]; 
+							   
+
+						 	}
+							
+
+
+						})
+
+			            
 			        }
     }
 
